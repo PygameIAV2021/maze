9b403ee5f2a52a5511309256c8ef3f5de2e813fa
Eicirius <jan.stark@strng.de>
2020-12-06 11:28:56 +0100

    updated generator

--- generator.py
+++ generator.py
@@ -9,15 +9,23 @@ Created on Thu Dec  3 08:27:00 2020
 import math 
 from enum import Enum
 
-class f_value(Enum):
+class field_v(Enum):
     NOT_VISITED = 0
-    VISITED = 1
-    NEIGHBOUR = 2
+    NORTH = 1
+    EAST = 2
+    SOUTH = 3
+    WEST = 4    
+    START = 5
     C_NORTH = 11
     C_EAST = 12
     C_SOUTH = 13
     C_WEST = 14
-    C_NEIGHBOUR = 15
+
+class return_v(Enum):
+    VALID = 0
+    OCCUPIED = 1
+    OUTOFBOUND = 2
+
 
 class generator():
     def __init__(self, xSize, ySize):
@@ -25,82 +33,42 @@ class generator():
         self.ySize = ySize
         # Two-dimensional list containing current maze
         self.field = [[0] * self.ySize for i in range(self.xSize)]  
+        # Two-dimensional list containing current maze including walls
+        self.walled_field = [[0] * (self.ySize * 3 - 2) for i in range(self.xSize * 3 - 2)]  
         # Remaining cells
-        self.remainingCells = range(0,self.ySize*self.xSize)
+        self.remainingCells = [range(0,self.ySize*self.xSize)]
         
     def generateMaze(self):
+        # Get starting cell
         start = self.remainingCells.pop()
         sx = math.floor(start/self.ySize)
-        sy = start%self.ySize# 03 = indirect neighbour (NE,SE,NW,SW)
-        self.fillCell(sx,sy)
+        sy = start%self.ySize
+        self.fillCell(field_v.START,sx,sy)
+        # While there are still unvisited cell do generate new cells
         while len(self.remainingCells) > 0:
             currentCellNr = self.remainingCells.pop()
             cx = math.floor(currentCellNr/self.ySize)
             cy = currentCellNr%self.ySize
             currentPath = self.randomWalk(cx,cy)    
-            for dx,dy in currentPath:
+            for dd,dx,dy in currentPath:
                 # Fill all cells visited by the current path
-                self.fillCell(dx,dy)
+                self.fillCell(dd, dx,dy)
                 # Remove all visited cells from the list of remaining cells
                 self.remainingCells.remove(dx*self.ySize + dy)
     
-    def fillPathCell(self, d, x, y):
+    def fillCell(self, d, x, y):
         # Set cell as visited
-        self.field[x][y] = d
-        # Set all neighbours as adjacent (2 for N, E, S, W; 3 for NE, SE, NW, SW) if they haven't visited before
-        if x+1 < self.xSize:
-            if self.field[x+1][y] == f_value.NOT_VISITED:
-                self.field[x+1][y] = f_value.C_NEIGHBOUR
-            if y+1 < self.ySize:
-                if self.field[x+1][y+1] == f_value.NOT_VISITED:
-                    self.field[x+1][y+1] == f_value.C_NEIGHBOUR    
-            if y-1 < self.ySize:
-                if self.field[x+1][y-1] == f_value.NOT_VISITED:
-                    self.field[x+1][y-1] == f_value.C_NEIGHBOUR 
-        if y+1 < self.ySize:
-            if self.field[x][y+1] == f_value.NOT_VISITED:
-                self.field[x][y+1] = f_value.C_NEIGHBOUR            
-        if x-1 >= 0:
-            if self.field[x-1][y] == f_value.NOT_VISITED:
-                self.field[x-1][y] == f_value.C_NEIGHBOUR
-            if y+1 < self.ySize:
-                if self.field[x-1][y+1] == f_value.NOT_VISITED:
-                    self.field[x-1][y+1] == f_value.C_NEIGHBOUR    
-            if y-1 < self.ySize:
-                if self.field[x-1][y-1] == f_value.NOT_VISITED:
-                    self.field[x-1][y-1] == f_value.C_NEIGHBOUR
-        if y-1 >= 0:
-            if self.field[x][y-1] == f_value.NOT_VISITED:
-                self.field[x][y-1] = f_value.C_NEIGHBOUR        
-            
-    def fillCell(self, x, y):
-        # Set cell as visited
-        self.field[x][y] = f_value.VISITED
-        # Set all neighbours as adjacent (2 for N, E, S, W; 3 for NE, SE, NW, SW) if they haven't visited before
-        if x+1 < self.xSize:
-            if self.field[x+1][y] == f_value.NOT_VISITED:
-                self.field[x+1][y] = f_value.NEIGHBOUR
-            if y+1 < self.ySize:
-                if self.field[x+1][y+1] == f_value.NOT_VISITED:
-                    self.field[x+1][y+1] == f_value.NEIGHBOUR    
-            if y-1 < self.ySize:
-                if self.field[x+1][y-1] == f_value.NOT_VISITED:
-                    self.field[x+1][y-1] == f_value.NEIGHBOUR 
-        if y+1 < self.ySize:
-            if self.field[x][y+1] == f_value.NOT_VISITED:
-                self.field[x][y+1] = f_value.NEIGHBOUR            
-        if x-1 >= 0:
-            if self.field[x-1][y] == f_value.NOT_VISITED:
-                self.field[x-1][y] == f_value.NEIGHBOUR
-            if y+1 < self.ySize:
-                if self.field[x-1][y+1] == f_value.NOT_VISITED:
-                    self.field[x-1][y+1] == f_value.NEIGHBOUR    
-            if y-1 < self.ySize:
-                if self.field[x-1][y-1] == f_value.NOT_VISITED:
-                    self.field[x-1][y-1] == f_value.NEIGHBOUR 
-        if y-1 >= 0:
-            if self.field[x][y-1] == f_value.NOT_VISITED:
-                self.field[x][y-1] = f_value.NEIGHBOUR  
+        self.field[x][y] = d     
+
+    def checkCell(self,x,y):
+        # Check if cell is empty and inside boundaries
+        if x > 0 and y > 0  and y < self.ySize and x < self.xSize:
+            if self.field[x][y] > 0:
+                return return_v.OCCUPIED
+            else:
+                return return_v.VALID
+        else:
+            return return_v.OUTOFBOUND
                         
     def randomWalk(self, startx, starty):
         return [[1,1],[1,2]]
